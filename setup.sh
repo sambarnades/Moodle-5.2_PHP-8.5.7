fc575aa8f961d3e081cdc9d870dbc92a4efc4193
Sam Barnades <samuel.barnades@gmail.com>
2026-03-02 14:40:14 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,48 @@
+# Update & install dependencies
+
+apt-get clean
+apt-get update -y
+apt-get install -y libfreetype-dev libjpeg62-turbo-dev libpng-dev
+
+
+# Install php extensions
+
+docker-php-ext-configure gd --with-freetype --with-jpeg
+docker-php-ext-install -j$(nproc) gd ctype curl dom iconv intl json mbstring pcre simplexml spl xml zip pgsql openssl sodium tokenizer xmlrpc
+
+# Install git
+
+apt-get install -y git
+
+# Create moodledata directory
+
+mkdir ./moodledata
+chmod 0777 ./moodledata
+
+# Download Moodle 5.1
+
+# Prod only --------------------------
+
+# git clone git://git.moodle.org/moodle.git
+
+# ------------------------------------
+
+chown -R root ./moodle
+chmod -R 0755 ./moodle
+
+ cd moodle
+ git branch --track MOODLE_501_STABLE origin/MOODLE_501_STABLE
+ git checkout MOODLE_501_STABLE
+
+
+find ./moodle -type f -exec chmod 0644 {} \;
+
+chown -R www-data:www-data /var/www/
+chmod -R 755 /var/www
+
+cat /etc/os-release
+
+php ./admin/cli/install.php
+
+
+# cp config-dist.php config.php
